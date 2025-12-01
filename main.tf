8547b988ddb0c860ed009d368fc343a69e8e8d84
Dessy-star <desolakanni@gmail.com>
2025-12-01 18:06:32 -0500

    uncommented aks code

--- main.tf
+++ main.tf
@@ -15,22 +15,21 @@ resource "azurerm_storage_account" "dessystorage" {
   }
 }
 
-#resource "azurerm_kubernetes_cluster" "dessyaks" {
- # name                = "dessy-aks1"
-  #location            = azurerm_resource_group.rg.location
-  #resource_group_name = azurerm_resource_group.rg.name
-  #dns_prefix          = "dessyaks1"
+resource "azurerm_kubernetes_cluster" "dessyaks" {
+  name                = "dessy-aks1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "dessyaks1"
 
-  #default_node_pool {
-   # name       = "default"
-    #node_count = 1
-    #vm_size    = "Standard_D2_v3"
-  #}
-  #identity {
-   # type = "SystemAssigned"
- # }
-
-  #tags = {
-   # Environment = "demo"
- # }
-#}
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v3"
+  }
+  identity {
+    type = "SystemAssigned"
+ }
+  tags = {
+    Environment = "demo"
+  }
+}
